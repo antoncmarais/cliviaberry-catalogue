14518a07b65f9844009d4e7b151e99cac6aacabb
antoncmarais <antoncmarais@gmail.com>
2026-04-25 22:39:58 +0200

    Add plant 458 Golden Fleece x Coromandel Breeding

--- plants.js
+++ plants.js
@@ -5352,5 +5352,17 @@ const PLANT_DATA = [
     "maturity": "2028",
     "breeder": "Chris Smit",
     "notes": "SL 2536"
+  },
+  {
+    "num": 458,
+    "name": "Golden Fleece x Coromandel Breeding",
+    "qty": 1,
+    "stock": 1,
+    "cost": 0.0,
+    "price": 500,
+    "type": "",
+    "maturity": "2028",
+    "breeder": "Chris Smit",
+    "notes": "SL 2540"
   }
 ];
